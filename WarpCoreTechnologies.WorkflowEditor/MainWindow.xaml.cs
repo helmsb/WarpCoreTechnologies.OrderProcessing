4ad9d85f8b68737f1b6806aa91a014bebe0581f6
bhelms <blake@blakehelms.net>
2016-01-31 18:53:24 -0600

    Initial project setup

---
+++ WarpCoreTechnologies.WorkflowEditor/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Core.Presentation;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Metadata;
+using System.Activities.Presentation.Toolbox;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarpCoreTechnologies.WorkflowEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WorkflowDesigner _wd;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Register the metadata
+            RegisterMetadata();
+
+            // Add the WFF Designer
+            AddDesigner();
+
+            AddToolBox();
+
+            AddPropertyInspector();
+
+        }
+
+        private void AddDesigner()
+        {
+            //Create an instance of WorkflowDesigner class.
+            this._wd = new WorkflowDesigner();
+
+            //Place the designer canvas in the middle column of the grid.
+            Grid.SetColumn(this._wd.View, 1);
+
+            //Load a new Sequence as default.
+            this._wd.Load(new Sequence());
+
+            //Add the designer canvas to the grid.
+            grid1.Children.Add(this._wd.View);
+        }
+
+        private void RegisterMetadata()
+        {
+            DesignerMetadata dm = new DesignerMetadata();
+            dm.Register();
+        }
+
+        private ToolboxControl GetToolboxControl()
+        {
+            // Create the ToolBoxControl.
+            ToolboxControl ctrl = new ToolboxControl();
+
+            // Create a category.
+            ToolboxCategory category = new ToolboxCategory("category1");
+
+            // Create Toolbox items.
+            ToolboxItemWrapper tool1 =
+                new ToolboxItemWrapper("System.Activities.Statements.Assign",
+                typeof(Assign).Assembly.FullName, null, "Assign");
+
+            ToolboxItemWrapper tool2 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
+                typeof(Sequence).Assembly.FullName, null, "Sequence");
+
+            // Add the Toolbox items to the category.
+            category.Add(tool1);
+            category.Add(tool2);
+
+            // Add the category to the ToolBox control.
+            ctrl.Categories.Add(category);
+            return ctrl;
+        }
+
+        private void AddToolBox()
+        {
+            ToolboxControl tc = GetToolboxControl();
+            Grid.SetColumn(tc, 0);
+            grid1.Children.Add(tc);
+        }
+
+        private void AddPropertyInspector()
+        {
+            Grid.SetColumn(_wd.PropertyInspectorView, 2);
+            grid1.Children.Add(_wd.PropertyInspectorView);
+        }
+
+    }
+}
